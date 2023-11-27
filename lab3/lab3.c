6cac20b2d2994aba86390accbe422210dd61c50e
Deniel Saskin <Deniel.saskin@gmail.com>
2023-11-27 14:07:59 +0100

    added test program to test singular numbers

--- lab3/lab3.c
+++ lab3/lab3.c
@@ -1,21 +1,37 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-    uint32_t testnumber;
-    uint32_t masked_number, page, offset;
+    FILE *file;
+    char input[10];
+
+    file = fopen("addresses.txt", "r");
+
+    uint32_t input_number;
+    uint32_t masked_number, offset, page;
     uint32_t initial_mask = 0xffff;
-    uint32_t page_mask = 0xff00;
-    uint32_t offset_mask = 0x00ff;
+    uint32_t offset_mask = 0xff00;
+    uint32_t page_mask = 0x00ff;
+    int page_table[256];
 
-    scanf("%d", &testnumber);
+    // scanf("%d", &input_number);
 
-    masked_number = testnumber & initial_mask;
-    page = masked_number & page_mask;
-    page = page >> 8;
-    offset = masked_number & offset_mask;
+    while (fgets(input, 10, file) != NULL)
+    {
+        input_number = atoi(input);
 
-    printf("the page number is %d \n", page);
-    printf("the offset number is %d \n", offset);
+        masked_number = input_number & initial_mask;
+        offset = masked_number & offset_mask;
+        offset = offset >> 8;
+        page = masked_number & page_mask;
+
+
+        printf("the page number is %d ", page);
+        printf("the offset number is %d \n", offset);
+    }
+
+    fclose(file);
+    return 0;
 }
