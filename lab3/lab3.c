de369f41c49ed8dd4e6a33e16beae256864429b3
Deniel Saskin <Deniel.saskin@gmail.com>
2023-11-27 14:20:44 +0100

    added comments

--- lab3/lab3.c
+++ lab3/lab3.c
@@ -4,32 +4,32 @@
 
 int main()
 {
-    FILE *file;
-    char input[10];
+    FILE *file;             //deklarerar bara filen. 
+    char input[10];         //char array för siffrorna i adresses.txt
 
-    file = fopen("addresses.txt", "r");
+    file = fopen("addresses.txt", "r"); //fil "adresses.txt" öppnad i "r" read mode
 
-    uint32_t input_number;
-    uint32_t masked_number, offset, page;
-    uint32_t initial_mask = 0xffff;
-    uint32_t offset_mask = 0xff00;
-    uint32_t page_mask = 0x00ff;
-    int page_table[256];
+    uint32_t input_number;                  //32 bitars int för input
+    uint32_t masked_number, offset, page;   //3 stycken 32 bitars int
+    uint32_t initial_mask = 0xffff;         //Mask för att få den 16 bitarna till höger
+    uint32_t offset_mask = 0xff00;          //Mask för att få den 8 vänstar bitarna
+    uint32_t page_mask = 0x00ff;            //Mask för att få den 8 högra bitarna
+    int page_table[256];                    //page table används inte ännu (itn array)
 
     // scanf("%d", &input_number);
 
-    while (fgets(input, 10, file) != NULL)
+    while (fgets(input, 10, file) != NULL)  //while det finns rader i "file" så läser vi in i "input" med längd "10" 
     {
-        input_number = atoi(input);
+        input_number = atoi(input);     //atoi konverterar string till int.
 
-        masked_number = input_number & initial_mask;
-        offset = masked_number & offset_mask;
-        offset = offset >> 8;
-        page = masked_number & page_mask;
+        masked_number = input_number & initial_mask; //första masken så ingen smuts hänger med. egentligen onödig
+        offset = masked_number & offset_mask;        //bitwise OR (& inte &&) för offset  
+        offset = offset >> 8;                        //pushar bitarna så vi får 0x00FF istället för 0xFF00. 
+        page = masked_number & page_mask;            //bitwise OR för page nummer
 
 
-        printf("the page number is %d ", page);
-        printf("the offset number is %d \n", offset);
+        printf("the page number is %d ", page);        //print decimalt pagenumret
+        printf("the offset number is %d \n", offset);   //print decimalt offset
     }
 
     fclose(file);
